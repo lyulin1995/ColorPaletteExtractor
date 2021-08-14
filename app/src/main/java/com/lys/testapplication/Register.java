158dd6c18d0f28daf3a82bc2c6e3cb45d541c52f
lyulin1995 <lyulin.lynn@gmail.com>
2021-08-14 02:12:03 +0200

    Register using email and send user to the main page

--- app/src/main/java/com/lys/testapplication/Register.java
+++ app/src/main/java/com/lys/testapplication/Register.java
@@ -1,17 +1,25 @@
 package com.lys.testapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Register extends AppCompatActivity {
 
     EditText registerFullName, registerEmail, registerPassword, registerConfPass;
     Button registerUserBtn, gotoLogin;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,8 @@ public class Register extends AppCompatActivity {
         registerConfPass = findViewById(R.id.confPassword);
         registerUserBtn = findViewById(R.id.registerBtn);
         gotoLogin = findViewById(R.id.gotoLogin);
+
+        fAuth = FirebaseAuth.getInstance();
 
         registerUserBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,6 +72,24 @@ public class Register extends AppCompatActivity {
                 // data is validated
                 // register the user using firebase
                 Toast.makeText(Register.this, "Data Validated", Toast.LENGTH_SHORT).show();
+
+                // Store the email and password
+                fAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        // send user to next page
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        finish();  // don't want user come back registration activity
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Register.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
 
 
 
