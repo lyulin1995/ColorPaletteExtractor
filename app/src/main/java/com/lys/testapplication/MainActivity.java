a81a68413d93286a7bfa85096cd45546c49ca8e6
lyulin1995 <lyulin.lynn@gmail.com>
2021-08-15 20:05:26 +0200

    Verify Email

--- app/src/main/java/com/lys/testapplication/MainActivity.java
+++ app/src/main/java/com/lys/testapplication/MainActivity.java
@@ -6,16 +6,48 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView verifyMsg;
+    Button verifyEmailBtn;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        auth = FirebaseAuth.getInstance();
+
         Button logout = findViewById(R.id.logoutBtn);
+        verifyMsg = findViewById(R.id.verifyEmailMsg);
+        verifyEmailBtn = findViewById(R.id.verifyEmailBtn);
+
+        if (!auth.getCurrentUser().isEmailVerified()){
+            verifyEmailBtn.setVisibility(View.VISIBLE);
+            verifyMsg.setVisibility(View.VISIBLE);
+        }
+
+        verifyEmailBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // send verification email
+                auth.getCurrentUser().sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(MainActivity.this, "Verification Email Sent", Toast.LENGTH_SHORT).show();
+                        verifyEmailBtn.setVisibility(View.GONE);
+                        verifyMsg.setVisibility(View.GONE);
+                    }
+                });
+            }
+        });
+
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
