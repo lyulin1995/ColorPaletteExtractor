ff1f1726cccf6898fc6abba66e38d46ebea8f0a9
lyulin1995 <lyulin.lynn@gmail.com>
2021-08-19 09:41:39 +0200

    forget password

--- app/src/main/java/com/lys/testapplication/Login.java
+++ app/src/main/java/com/lys/testapplication/Login.java
@@ -1,10 +1,14 @@
 package com.lys.testapplication;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,12 +18,16 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
     // create variables for button, plain text...
-    Button createAccoutBtn, loginBtn;
+    Button createAccountBtn, loginBtn, forget_password_btn;
     EditText username, password;
     FirebaseAuth firebaseAuth;
+    AlertDialog.Builder reset_alert;
+    LayoutInflater inflater;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +36,13 @@ public class Login extends AppCompatActivity {
 
         firebaseAuth = FirebaseAuth.getInstance();
 
-        createAccoutBtn = findViewById(R.id.createAccountBtn);
-        createAccoutBtn.setOnClickListener(new View.OnClickListener() {
+        reset_alert = new AlertDialog.Builder(this);
+        inflater = this.getLayoutInflater();
+
+
+
+        createAccountBtn = findViewById(R.id.createAccountBtn);
+        createAccountBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), Register.class));
@@ -39,6 +52,48 @@ public class Login extends AppCompatActivity {
         username = findViewById(R.id.loginEmail);
         password = findViewById(R.id.loginPassword);
         loginBtn = findViewById(R.id.loginbtn);
+        forget_password_btn = findViewById(R.id.forget_password_btn);
+
+        forget_password_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // start alertdialog
+                View view = inflater.inflate(R.layout.reset_pop, null);
+
+                reset_alert.setTitle("Reset Forgot Password ?").setMessage("Enter Your Email to get Password Reset link.")
+                        .setPositiveButton("Reset", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // validate the email address
+                                EditText email = view.findViewById(R.id.reset_email_pop);
+                                if (email.getText().toString().isEmpty()){
+                                    email.setError("Required Field");
+                                    return;
+                                }
+                                // send the reset link
+                                firebaseAuth.sendPasswordResetEmail(email.getText().toString()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Toast.makeText(Login.this, "Reset Email Sent", Toast.LENGTH_SHORT).show();
+
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(Login.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                                    }
+                                });
+
+
+                            }
+                        }).setNegativeButton("Cancel", null)
+                        .setView(view)
+                        .create().show();
+
+
+            }
+        });
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
