1dea4f524baa62454326a8981fa160a88687e1a2
lyulin1995 <lyulin.lynn@gmail.com>
2021-08-19 10:00:07 +0200

    Update Email

--- app/src/main/java/com/lys/testapplication/Login.java
+++ app/src/main/java/com/lys/testapplication/Login.java
@@ -85,7 +85,6 @@ public class Login extends AppCompatActivity {
                                     }
                                 });
 
-
                             }
                         }).setNegativeButton("Cancel", null)
                         .setView(view)
